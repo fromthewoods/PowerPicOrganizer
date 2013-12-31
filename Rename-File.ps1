a11fe8334a6a8cf73ccfaaa5affdd1091647a0d9
fromthewoods <jbaltrus@gmail.com>
2013-12-31 15:13:58 -0700

    Initial Commit

---
+++ Rename-File.ps1
@@ -0,0 +1,128 @@
+﻿<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+
+#[CmdletBinding()]
+#Param
+#(
+#    # The source path containing files to be renamed.
+#    [Parameter(Mandatory=$true,
+#               ValueFromPipelineByPropertyName=$true,
+#               Position=0)]
+#    $Path = "E:\Pictures\Camera\"
+#,
+#    # The destination directory where the renamed files will be placed.
+#    [Parameter(Mandatory=$true,
+#               Position=1)]
+#    $Destination = "E:\Pictures\Camera\Testing\"
+#,
+#    # Specify the extension of the files to be renamed
+#    [Parameter(Mandatory=$true,
+#               Position=2)]
+#    [ValidateSet("*.jpg")]
+#    $Extension = "*.jpg"
+#,
+#    [switch]$DeleteSourceFiles
+#)
+
+$Path = "E:\Pictures\Camera\"
+$Destination = "E:\Pictures\Camera\Testing\"
+$Extension = "*.jpg"
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+function Verb-Noun
+{
+    [CmdletBinding()]
+    [OutputType([int])]
+    Param
+    (
+        # Param1 help description
+        [Parameter(Mandatory=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=0)]
+        $File,
+
+        # Param2 help description
+        [int]
+        $Param2
+    )
+
+    Begin
+    {
+    }
+    Process
+    {
+        $prefix = $File.LastWriteTime.ToString('yyyyMMdd_HHmmss')
+        $Name = $prefix + $File.Name
+    }
+    End
+    {
+    }
+}
+
+<#
+.Synopsis
+   Returns an single item of Generic Exif data
+.Description
+   Returns the data part of a single EXIF property item -not the type. 
+.Example
+   C:\PS> Get-ExifItem -image $image -ExifID $ExifIDModel
+   Returns the Camera model string
+.Parameter image
+   The image from which the data will be read 
+.Parameter ExifID
+   The ID of the required data field. 
+   The module defines constants with names beginning $ExifID for the most used ones. 
+#>
+Function Get-ExifItem
+{
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        [__ComObject]
+        $Image
+    ,
+        [Parameter(Mandatory=$true)]
+        $ExifID = "36867"
+    )
+
+    Process
+    {
+        Foreach ($ID in $ExifID) {
+            Try   {$item = $Image.Properties.Item("$ID")  }
+            Catch { Write-Verbose "Error getting exif item $ID - probably doesn't exist"  ; continue   }
+            If ($item) {
+                Write-Verbose "Type is $($item.type)"
+                # "Rational"=1006;"URational"=1007
+                #If (($item.Type -eq 1007) -or ($item.Type -eq 1006) ) {
+                #    If (($ExifID -eq $ExifIDExposuretime) -and ($item.Value.Numerator -eq 1)) {"1/$($item.Value.Denominator)"} 
+                #    Else {$item.value.value}
+                #}
+                # "VectorOfByte"=1101
+                #ElseIf (($item.type -eq 1101) -or ($item.type -eq 1100)) {$item.value.string() }
+                #Else {
+                    $item.value
+                #}
+            }
+        }
+    }
+}
+
+(gci -Path $Path -Filter $Extension).FullName # | Get-ExifItem
---
+++ Untitled1.ps1
@@ -0,0 +1,111 @@
+﻿<#
+.Synopsis
+   Returns an single item of Generic Exif data
+.DESCRIPTION
+   Returns the data part of a single EXIF property item -not the type. 
+.EXAMPLE
+   Get-ExifItem -ImageFile "E:\Pictures\Camera\Testing\Date.jpg" -ExifID 36867
+   Returns the Camera model string
+.EXAMPLE
+   (gci -Path "E:\Pictures\Camera\Testing").FullName | Get-ExifItem -ExifID 36867
+.Parameter ImageFile
+   The image from which the data will be read 
+.Parameter ExifID
+   The ID of the required data field. 
+   The module defines constants with names beginning $ExifID for the most used ones.
+   List of ID's and definitions: http://msdn.microsoft.com/en-us/library/ms534416.aspx 
+#>
+
+Function Get-ExifItem
+{
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        $ImageFile
+    ,
+        [Parameter(Mandatory=$true)]
+        $ExifID
+    )
+    Process
+    {
+        $FileStream=New-Object System.IO.FileStream($ImageFile,
+                                                    [System.IO.FileMode]::Open,
+                                                    [System.IO.FileAccess]::Read,
+                                                    [System.IO.FileShare]::Read,
+                                                    1024,     # Buffer size
+                                                    [System.IO.FileOptions]::SequentialScan
+                                                    )
+        $Img = [System.Drawing.Imaging.Metafile]::FromStream($FileStream)
+        Try { 
+            $ExifDT = $Img.GetPropertyItem($ExifID)
+        } Catch {
+            #$_.Exception.GetType().FullName
+            Write-Output "Error getting exif item $ExifID - probably doesn't exist"
+            Return $false
+        }
+
+        $ExifDtString=[System.Text.Encoding]::ASCII.GetString($ExifDT.Value)
+                
+        $DateTime=[datetime]::ParseExact($ExifDtString,"yyyy:MM:dd HH:mm:ss`0",$Null)
+        $FileStream.Close(); $Img.Dispose()
+        Return $DateTime
+    }
+}
+
+<#
+.Synopsis
+   Short description
+.DESCRIPTION
+   Long description
+.EXAMPLE
+   Example of how to use this cmdlet
+.EXAMPLE
+   Another example of how to use this cmdlet
+#>
+Function Get-ImageDateTaken
+{
+    Param
+    (
+        [Parameter(Mandatory=$true,
+                   ValueFromPipeline=$true)]
+        $fileName
+    )
+    Process
+    {
+        $date = Get-ExifItem -ImageFile $fileName -ExifID 36867
+        If ($date) {Return $date.ToString('yyyyMMdd_HHmmss')}
+    }
+}
+
+(gci -Path "E:\Pictures\Camera\Testing").FullName | Get-ImageDateTaken
+ ##$dateTakenForFilename = GetDateTakenForFilename("D:\videos\test\153-P1040727.jpg")
+##echo $dateTakenForFilename
+
+ Function PrefixAllJpegsWithDateTaken($folderToRenameFilesIn)
+{
+    foreach ($filepath in [System.IO.Directory]::GetFiles($folderToRenameFilesIn))
+    {
+        $file = New-Object System.IO.FileInfo($filepath);
+        $filename = $file.Name
+        
+        $datePrefix = $file.LastWriteTime.ToString('yyyyMMdd_HHmmss')
+        
+        if ($filename.EndsWith(".jpg"))
+        {
+           $dateTaken = GetDateTakenForFilename($filepath)
+           echo "Date Taken: $dateTaken"
+           $datePrefix = $dateTaken
+        }
+        
+        $targetPath = $folderToRenameFilesIn + '\' + $datePrefix + $filename
+        echo $targetPath
+        [System.IO.File]::Copy($filepath, $targetPath)
+       # $newfile = New-Object System.IO.FileInfo($targetPath);
+       # $newCreationDate = $newfile.CreationTime.ToString('yyyyMMdd-hhmmss')
+       # $newLastWriteTime = $newfile.LastWriteTime.ToString('yyyyMMdd-hhmmss') 
+       # echo "Original Filename: $filename  Creation Time: $date  Last Write Time:  $lastWriteTime Target Path: $targetPath New File Creation Time: $newCreationDate New File Last Write Time:  $newLastWriteTime"
+    }
+}
+
+#PrefixAllJpegsWithDateTaken("d:\videos\test")
