69318c4b1fdf1355daf634546aec1d6b9c3867ec
Joel Baltrus <jbaltrus@gmail.com>
2020-03-19 21:31:53 -0700

    Init Pester for Get-Images

---
+++ source/Tests/Get-Images.Tests.ps1
@@ -0,0 +1,9 @@
+﻿$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+$sut = (Split-Path -Leaf $MyInvocation.MyCommand.Path) -replace '\.Tests\.', '.'
+. "$here\..\Public\$sut"
+
+Describe "Get-Images" {
+    It "does something useful" {
+        $true | Should -Be $false
+    }
+}
